3aa08f9424b769bf25b4f19e24d2937bdbec7eaf
Hint-SocialSpace <hintinfo.team@gmail.com>
2025-11-12 23:23:52 +0100

    file commit

---
+++ index.js
@@ -0,0 +1,23 @@
+export class Router{
+    constructor({ routes, render }) {
+        this.routes = routes;
+        this.render = render;
+        document.addEventListener('popstate', () => resolve());
+        this.resolve()
+    }
+    
+    navigate(path) {
+        history.pushState({}, '', path);
+        this.resolve();
+    }
+    
+    resolve() {
+        const path = location.pathname;
+        const route = this.routes.find(r => r.path === path);
+     if (route && typeof window !== 'undefined') {
+          this.render(route.component)
+        } else {
+            this.render('404 - Page Not Found');
+        }
+    }
+}
